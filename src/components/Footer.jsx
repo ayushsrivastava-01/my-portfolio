ef486420872d4cba001ec199647a9c4665237bfc
Ayush Srivastava <ayushsrivastava1854@gmail.com>
2025-08-30 05:18:20 +0530

    Social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,33 +2,33 @@ import React from 'react';
 import './css/Footer.css';
 import { FaInstagram, FaLinkedin, FaTelegram, FaGithub } from 'react-icons/fa';
 import { FaThreads } from 'react-icons/fa6';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="footer-section">
       <div className="container">
         <div className="footer-content">
-          {/* Left - Name & Profession */}
+          {/* Left */}
           <div className="footer-left">
             <h2 className="footer-heading-underline">Ayush Srivastava</h2>
             <p>Full-Stack Developer</p>
             <p>Programmer</p>
           </div>
 
-          {/* Center - Quick Links in boxes */}
+          {/* Center - Quick Links */}
           <div className="footer-center">
             <h4 className="footer-heading-underline">Quick Links</h4><br />
             <div className="quick-links-boxes">
-              <Link to="/" className="quick-box">Home</Link>
-              <Link to="/about" className="quick-box">About</Link>
-              <Link to="/skills" className="quick-box">Skills</Link>
-              <Link to="/certificate" className="quick-box">Certificates</Link>
-              <Link to="/resume" className="quick-box">Resume</Link>
+              <NavLink to="/" className="quick-box">Home</NavLink>
+              <NavLink to="/about" className="quick-box">About</NavLink>
+              <NavLink to="/skills" className="quick-box">Skills</NavLink>
+              <NavLink to="/certificate" className="quick-box">Certificates</NavLink>
+              <NavLink to="/resume" className="quick-box">Resume</NavLink>
             </div>
           </div>
 
-          {/* Right - Social Icons and Email */}
+          {/* Right */}
           <div className="footer-right">
             <h4 className="footer-heading-underline">Connect with Me</h4>
             <div className="social-icons">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './css/navbar.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -15,7 +15,7 @@ const Navbar = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  // Close menu on navigation (optional)
+  // Close menu on navigation
   const handleNavClick = () => {
     setIsOpen(false);
   };
@@ -23,7 +23,9 @@ const Navbar = () => {
   return (
     <nav className={`navbar navbar-expand-lg custom-navbar sticky-top ${scrolled ? 'sticky-scrolled' : ''}`}>
       <div className="container">
-        <Link className="navbar-brand" to="/" onClick={handleNavClick}>Ayush's Portfolio</Link>
+        <NavLink className="navbar-brand" to="/" onClick={handleNavClick}>
+          Ayush's Portfolio
+        </NavLink>
 
         <button
           className={`navbar-toggler custom-toggler ${isOpen ? 'open' : ''}`}
@@ -33,7 +35,7 @@ const Navbar = () => {
           aria-label="Toggle navigation"
           onClick={() => setIsOpen(!isOpen)}
         >
-          {/* Hamburger icon lines */}
+          {/* Hamburger icon */}
           <span className="toggler-icon"></span>
           <span className="toggler-icon"></span>
           <span className="toggler-icon"></span>
@@ -42,31 +44,25 @@ const Navbar = () => {
         <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
           <ul className="navbar-nav ms-auto d-flex align-items-center flex-column flex-lg-row text-center text-lg-start">
             <li className="nav-item">
-              <Link className="nav-link custom-link" to="/" onClick={handleNavClick}>Home</Link>
+              <NavLink className="nav-link custom-link" to="/" onClick={handleNavClick}>Home</NavLink>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link custom-link" to="/about" onClick={handleNavClick}>About</Link>
+              <NavLink className="nav-link custom-link" to="/about" onClick={handleNavClick}>About</NavLink>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link custom-link" to="/skills" onClick={handleNavClick}>Skills</Link>
+              <NavLink className="nav-link custom-link" to="/skills" onClick={handleNavClick}>Skills</NavLink>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link custom-link" to="/projects" onClick={handleNavClick}>Projects</Link>
+              <NavLink className="nav-link custom-link" to="/projects" onClick={handleNavClick}>Projects</NavLink>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link custom-link" to="/contact" onClick={handleNavClick}>Contact</Link>
+              <NavLink className="nav-link custom-link" to="/contact" onClick={handleNavClick}>Contact</NavLink>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link custom-link" to="/certificate" onClick={handleNavClick}>Certificates</Link>
+              <NavLink className="nav-link custom-link" to="/certificate" onClick={handleNavClick}>Certificates</NavLink>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link custom-link" to="/resume" onClick={handleNavClick}>Resume</Link>
+              <NavLink className="nav-link custom-link" to="/resume" onClick={handleNavClick}>Resume</NavLink>
             </li>
           </ul>
         </div>
